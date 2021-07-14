20e54fbe18892e55792a30c2672839263b554da5
Shadow Killer <perirohan@gmail.com>
2021-07-14 00:52:08 -0700

    Final changes

--- main.c
+++ main.c
@@ -4,19 +4,22 @@
 #include "random.h"
 
 // Creating data tracking arrays for Monitors
-int sent1;
-int sent2;
+int sent1 = 0;
+int sent2 = 0;
 
-int received1;
-int received2;
+int received1 = 0;
+int received2 = 0;
 
-int overflow1;
-int overflow2;
+int overflow1 = 0;
+int overflow2 = 0;
 
 
 // Initializing the Queues
 osMessageQueueId_t q_1;
 osMessageQueueId_t q_2;
+
+int server1 = 1;
+int server2 = 2;
 
 //The actual message to send
 
@@ -25,24 +28,25 @@ int message = 420;
 //Client function
 void client(void *arg) {
 	
-	//DELAY
+	int serverDelay = 0;
+	int ticksToSeconds = osKernelGetTickFreq();
 	
-	int delay_s = 0;
-	delay_s = next_event();
 	
-	//gets ticks per second
-	int ticks_p_s = osKernelGetTickFreq();
-	
-	int avg_ticks = 0;
-	int nticks = 0;
-	avg_ticks = (delay_s*ticks_p_s)/9;
-	nticks = avg_ticks >> 16;
-	
-	//Infinite loop
+	//Infinite Loop
 	while(true){
 		
+		//DELAY
+		// Use the next_event function from the random file to get delay in seconds
+		serverDelay = next_event();
+		//Convert from seconds to ticks by multiplying by ticks/second
+		serverDelay = serverDelay*ticksToSeconds;
+		// Divide by our average rate for a server, which is 10Hz
+		serverDelay = serverDelay/9;
+		//Right shift the delay by 16 bits
+		serverDelay = serverDelay >> 16;
+		
 		//for queue 1
-		osDelay(nticks);
+		osDelay(serverDelay);
 		
 		osStatus_t status1;
 		status1 = osMessageQueuePut(q_1, &message, 0, 0);
@@ -56,7 +60,8 @@ void client(void *arg) {
 		}
 		
 		//for queue 2
-		osDelay(nticks);
+		osDelay(serverDelay);
+		
 		osStatus_t status2;
 		status2 = osMessageQueuePut(q_2, &message, 0, 0);
 		if (status2 == osOK)
@@ -71,7 +76,9 @@ void client(void *arg) {
 	
 }
 
-void servers(int server) {
+void servers(void *argument) {
+	int *serverPtr = (int *) argument;
+	int server = *serverPtr;
 	int serverDelay = 0;
 	int ticksToSeconds = osKernelGetTickFreq();
 	while(true) {
@@ -98,20 +105,51 @@ void servers(int server) {
 	}
 }
 
-void monitor() {
+void serverStart() {
+	// Initializing our queues
+	q_1 = osMessageQueueNew(10,sizeof(int),NULL);
+	q_2 = osMessageQueueNew(10,sizeof(int),NULL);
+	
+	//Starting up the server threads
+	osThreadNew(servers, &server1,NULL);
+	osThreadNew(servers, &server2,NULL);
+}
 
+void monitor() {
+	
+	//number of ticks in a second
+	int ticks_p_s = osKernelGetTickFreq();
+	//for reference, setting seconds to 1
+	int sec = 1;
+	
+	while(true){
+		//delay for one second
+		osDelay(ticks_p_s);
+		
+		//Displaying information
+		printf("Elapsed Time: %d ", sec);
+		
+		printf("Sent Queue 1: %d ", sent1);
+		printf("Sent Queue 2: %d ", sent2);
+		printf("Received Queue 1: %d ", received1);
+		printf("Received Queue 2: %d ", received2);
+		printf("Overflow Queue 1: %d ", overflow1);
+		printf("Overflow Queue 2: %d ", overflow2);
+		printf("\n");
+		
+		sec++;
+	}
 }
 
 
 int main(){
 	osKernelInitialize();
 	
+	serverStart();
+	
 	osThreadNew(client, NULL, NULL);
 	
-	// Figure out how to send arguments 
-	osThreadNew(servers, NULL, NULL);
-	osThreadNew(servers, NULL, NULL);
-	
 	osThreadNew(monitor, NULL, NULL);
+
 	osKernelStart();
 }
